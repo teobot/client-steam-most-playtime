05bc40e5130cd78655f878b3455e49b6ef31bb0e
teobot <theoclapperton@outlook.com>
2021-10-01 11:59:12 +0100

    Update for final

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+const DEV = false;
+
 const AxiosInstance = () => {
   const instance = axios.create({
-    baseURL: "http://localhost:3333/api/v1",
+    baseURL: DEV ? "http://localhost:3333/api/v1" : "https://api-teobot-steam-review.herokuapp.com/api/v1",
   });
 
   return instance;
